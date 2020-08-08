887b26dff39b860a9e1f72d75dac99c8293ce797
Bhavesh Chavda <Bsmarty@outlook.com>
2020-08-08 19:13:40 +0530

    first commit

---
+++ liquipedia/spiders/upcoming.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class UpcomingSpider(scrapy.Spider):
+    name = 'upcoming'
+    allowed_domains = ['liquipedia.net']
+    start_urls = ['https://liquipedia.net/rainbowsix/Portal:Tournaments']
+
+    def parse(self, response):
+        sections = response.xpath("//div[@class = 'divRow']") 
+        for row in sections:
+                yield{
+                    'title': row.xpath(".//b/a/text()").get(),
+                    'link': f'https://liquipedia.net{row.xpath(".//b/a/@href").get()}',
+                    'date': row.xpath(".//div[@class = 'divCell EventDetails-Left-55 Header']/text()").get(),
+                    'location': row.xpath(".//div[@class = 'divCell EventDetails-Left-60 Header']/text()").get(),
+                    'price': row.xpath(".//div[@class = 'divCell EventDetails-Right-45 Header']/text()").get(),
+                }
+        
+        
